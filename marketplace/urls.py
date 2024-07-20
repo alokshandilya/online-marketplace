16311ac93f0faeddf2759594221cad6bda7f9d34
alokshandilya <alok.shandilya10@gmail.com>
2024-07-21 05:03:20 +0530

    `/` homepage
    - add index.html
    - add index in views.py

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,8 +1,4 @@
 """
-URL configuration for marketplace project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,7 +13,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.index, name="index")
 ]
