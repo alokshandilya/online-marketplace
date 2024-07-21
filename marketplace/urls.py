8c6ca8e53b63d2ebe535781f3f41431c80c27e9f
Alok Shandilya <alok.shandilya10@gmail.com>
2024-07-21 05:47:35 +0530

    add contact
    - contact.html
    - contact view

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from core import views
+from core.views import index, contact
 
 urlpatterns = [
+    path("", index, name="index"),
+    path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
-    path("", views.index, name="index")
 ]
