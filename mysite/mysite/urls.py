a4ab500f1beb471f35ae53e2d9994df682049509
Alok Shandilya <alok.shandilya10@gmail.com>
2024-07-24 16:07:39 +0530

    fix django structure
    - rename app marketplace to mysite
    - remove app: item

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,4 +1,8 @@
 """
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,10 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from core.views import index, contact
+from core.views import index
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
+    path("", index, name="index"),
 ]
